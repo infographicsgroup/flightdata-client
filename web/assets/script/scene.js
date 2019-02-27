4c99144d6704aa7134ea2466735554413c618634
Frank Reitberger <frank@prinzipiell.com>
2019-02-27 20:05:27 +0100

    ToDo: remove delay when statecontroller is modified
    + delay helps at the moment to see whats and when its going on

--- web/assets/script/scene.js
+++ web/assets/script/scene.js
@@ -139,7 +139,7 @@ FlightGlobal.Scene = function (wrapper) {
 			stateController.showAirport(airport);
 
 			var currentFov = { fov: 10};
-        	TweenLite.to( currentFov, 0.5, { delay:0.2, fov:45, onUpdate:me.updateFov, onUpdateParams:[ currentFov ], ease:Expo.easeOut } );   
+        	TweenLite.to( currentFov, 0.5, { delay:0.5, fov:45, onUpdate:me.updateFov, onUpdateParams:[ currentFov ], ease:Expo.easeOut } );   
 		},
 		
 		resize: resize,
@@ -157,7 +157,7 @@ FlightGlobal.Scene = function (wrapper) {
 			globe.clickableObjects.push(airport.marker);
 			airport.marker.onClick = function () {						
 				var currentFov = { fov: 45};
-        		TweenLite.to( currentFov, 0.5, { fov:10, onUpdate:me.updateFov, onUpdateParams:[ currentFov ], onComplete:me.onCompleteFov, onCompleteParams:[airport], ease: Back.easeOut } );   
+        		TweenLite.to( currentFov, 0.5, { fov:10, onUpdate:me.updateFov, onUpdateParams:[ currentFov ], onComplete:me.onCompleteFov, onCompleteParams:[airport], ease:Expo.easeOut } );   
 			}
 		})
 	}
