4be52ef4cf4dab499e60669ff15c50fbf0c70edc
Frank Reitberger <frank@prinzipiell.com>
2019-02-27 22:37:25 +0100

    Prepare: Sprite-Layer for labels
    + !!! needs to be a separate scene to  prevent being part of the main-postro-pipeline

--- web/assets/script/scene.js
+++ web/assets/script/scene.js
@@ -9,6 +9,8 @@ FlightGlobal.Scene = function (wrapper) {
 	//scene.background = new THREE.Color( 0x0c1a22 );
 	scene.background = new THREE.TextureLoader().load('assets/texture/background.png');
 
+	var labelScene = new THREE.Scene();
+
 	var camera = new THREE.PerspectiveCamera(45, 1, 0.01, 1000);
 	camera.position.set(0,0,3);
 
@@ -173,9 +175,16 @@ FlightGlobal.Scene = function (wrapper) {
 
 		requestAnimationFrame(render);
 		if (globe.object3D.visible) {
+
 			globeComposer.render(1 / 60);
+			//renderer.clearDepth();
+        	//renderer.render( labelScene, camera )
+
 		} else if ( !oneTime ) {
+			//renderer.clear();
 			globeComposer.render(1 / 60);
+			//renderer.clear();
+        	//renderer.render( labelScene, camera )
 			oneTime = true;
 		}
 		if (airportGroup && airportGroup.control && oneTime) {
