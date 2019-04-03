67699e8b132b9d69de9d9f9c899f2e2c50465ab5
Michael Kreil <github@michael-kreil.de>
2019-04-03 17:13:56 +0200

    move code

--- web/assets/script/helper.js
+++ web/assets/script/helper.js
@@ -4,6 +4,7 @@ FlightGlobal.helper = (function () {
 	return {
 		series:series,
 		diffDecoding:diffDecoding,
+		touchEvents:checkTouchEvents(),
 	}
 
 	function series(list) {
@@ -24,4 +25,19 @@ FlightGlobal.helper = (function () {
 		}
 	}
 
+	function checkTouchEvents() {
+		var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+		var mq = function(query) {
+			return window.matchMedia(query).matches;
+		}
+
+		if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+			return true;
+		}
+
+		// include the 'heartz' as a way to have a non matching MQ to help terminate the join
+		// https://git.io/vznFH
+		var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+		return mq(query);
+	}
 })()
--- web/assets/script/main.js
+++ web/assets/script/main.js
@@ -7,8 +7,7 @@ var FlightGlobal = {};
 $(function () {
 	var scene, airports, containerWidth, containerHeight;
 
-	var touchEvents = checkTouchEvents();
-	if (touchEvents) $('body').addClass('nohover');
+	if (FlightGlobal.helper.touchEvents) $('body').addClass('nohover');
 
 	var resizeTimeout = false;
 	$(window).resize(resize);
@@ -163,19 +162,3 @@ $(function () {
 		},
 	])
 })
-
-function checkTouchEvents() {
-	var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-	var mq = function(query) {
-		return window.matchMedia(query).matches;
-	}
-
-	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-		return true;
-	}
-
-	// include the 'heartz' as a way to have a non matching MQ to help terminate the join
-	// https://git.io/vznFH
-	var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-	return mq(query);
-}
