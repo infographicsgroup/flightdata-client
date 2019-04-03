55b0a13ed9cf978ec1001f4f598102a637163062
Michael Kreil <github@michael-kreil.de>
2019-04-03 16:52:18 +0200

    check for touch events

--- web/assets/script/main.js
+++ web/assets/script/main.js
@@ -7,6 +7,7 @@ var FlightGlobal = {};
 $(function () {
 	var scene, airports, containerWidth, containerHeight;
 
+	var touchEvents = checkTouchEvents();
 	var resizeTimeout = false;
 	$(window).resize(resize);
 	resize();
@@ -160,3 +161,19 @@ $(function () {
 		},
 	])
 })
+
+function checkTouchEvents() {
+	var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+	var mq = function(query) {
+		return window.matchMedia(query).matches;
+	}
+
+	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+		return true;
+	}
+
+	// include the 'heartz' as a way to have a non matching MQ to help terminate the join
+	// https://git.io/vznFH
+	var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+	return mq(query);
+}
